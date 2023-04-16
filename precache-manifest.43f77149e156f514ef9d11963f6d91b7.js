2eacc6c2cdc667b4b6bac859bad7b0f9535f6767
satyam-bidve <satyambidve@gmail.com>
2023-04-16 12:39:07 +0530

    Updates

---
+++ precache-manifest.43f77149e156f514ef9d11963f6d91b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "32352e6f63154213d51c",
+    "url": "/contact_keeper/static/css/main.07e7043c.chunk.css"
+  },
+  {
+    "revision": "32352e6f63154213d51c",
+    "url": "/contact_keeper/static/js/main.32352e6f.chunk.js"
+  },
+  {
+    "revision": "816ab2d2d6995f4b5451",
+    "url": "/contact_keeper/static/js/1.816ab2d2.chunk.js"
+  },
+  {
+    "revision": "a9607efe8baa19cb600b",
+    "url": "/contact_keeper/static/js/runtime~main.a9607efe.js"
+  },
+  {
+    "revision": "5762c40261cbf2b761b120a2dbcf364b",
+    "url": "/contact_keeper/static/media/user.5762c402.png"
+  },
+  {
+    "revision": "03cb479d9a42f936283913d364027468",
+    "url": "/contact_keeper/index.html"
+  }
+];
